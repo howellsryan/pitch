c6a32ea548bcca78f7a3b9175f028f61892ab212
howellsryan <146009227+howellsryan@users.noreply.github.com>
2026-09-01 19:15:25 +0100

    Load P5 responsive polish after existing UI styles

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import './r6.css';
 import './mobile-ux.css';
 import './r7.css';
 import './r7-mobile-fixes.css';
+import './p5.css';
 
 import { mount } from 'svelte';
 import { navigateTo } from './ui/helpers.js';
